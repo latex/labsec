98678982d2e0d823e584a0780fc45c517c0e08e8
LATex <leandro.recebe@gmail.com>
2025-10-13 12:52:55 -0300

    test: Adiciona testes para cenários de erro e validação
    - Adiciona testes para busca com palavra-chave inválida
    - Adiciona testes para limite inválido de CVEs
    - Adiciona testes para erro de API
    - Implementa validação de parâmetros

--- omama-cli/omama-cli.Tests/obj/Debug/net9.0/omama-cli.Tests.AssemblyInfo.cs
+++ omama-cli/omama-cli.Tests/obj/Debug/net9.0/omama-cli.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("omama-cli.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efbc4fe391a4ebe1a1c7142b5bd9171b53b87ebc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cc1f5f6022e9260b9bf060596a0087969b2574a")]
 [assembly: System.Reflection.AssemblyProductAttribute("omama-cli.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("omama-cli.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
